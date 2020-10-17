342be9e12caac0df60fa3adb7abc1b2ad04d9df2
Trong Nhan Mai <trongnhan061199@gmail.com>
2020-10-17 12:16:07 +1000

    Added basic UI

--- TextToPolyRhythmSpeech/PolyRhythmSpeechHome.xaml.cs
+++ TextToPolyRhythmSpeech/PolyRhythmSpeechHome.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Speech.Synthesis;
 
 namespace TextToPolyrhythmSpeech
 {
@@ -20,9 +21,43 @@ namespace TextToPolyrhythmSpeech
     /// </summary>
     public partial class PolyRhythmSpeechHome : Page
     {
+
         public PolyRhythmSpeechHome()
         {
             InitializeComponent();
         }
-    }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+			/*			PromptBuilder promptBuilder = new PromptBuilder();
+						promptBuilder.AppendText("Hello world");
+
+						PromptStyle promptStyle = new PromptStyle();
+						promptStyle.Volume = PromptVolume.Soft;
+						promptStyle.Rate = PromptRate.Slow;
+						promptBuilder.StartStyle(promptStyle);
+						promptBuilder.AppendText("and hello to the universe too.");
+						promptBuilder.EndStyle();
+
+						promptBuilder.AppendText("On this day, ");
+						promptBuilder.AppendTextWithHint(DateTime.Now.ToShortDateString(), SayAs.Date);
+
+						promptBuilder.AppendText(", we're gathered here to learn");
+						promptBuilder.AppendText("all", PromptEmphasis.Strong);
+						promptBuilder.AppendText("about");
+						promptBuilder.AppendTextWithHint("WPF", SayAs.SpellOut);*/
+
+			// Just getting the selected rythm here
+			ListBoxItem selected = (ListBoxItem)rythm1.SelectedItem;
+			String rythm = (String)selected.Content;
+			
+			// Rap the text
+			String text = InputTextBox.Text;
+			SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+			PromptBuilder promptBuilder = new PromptBuilder();
+			promptBuilder.AppendText(text);
+			speechSynthesizer.Speak(promptBuilder);
+		}
+
+	}
 }
